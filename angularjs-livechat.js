10d64d518de75954c988a2bc0f73e02e1a3f7510
wmedynski <ggmedi@o2.pl>
2017-11-23 12:38:04 +0100

    force https

--- angularjs-livechat.js
+++ angularjs-livechat.js
@@ -4,41 +4,41 @@
     define(['angular'], function(angular) {
       return factory(root, angular, root.__lc);
     });
-  }
-  // CommonJS and Node.js module support
-  else if (typeof exports !== 'undefined') {
+  } else if (typeof exports !== 'undefined') {
+    // CommonJS and Node.js module support
     exports = module.exports = factory({}, require('angular'), root.__lc);
-  }
-  // Angular
-  else if (angular) {
+  } else if (angular) {
+    // Angular
     factory(root, root.angular, root.__lc);
   }
-}(this, function(global, angular, __lc) {
+})(this, function(global, angular, __lc) {
   'use strict';
 
-  angular
-    .module('ngLiveChat', [])
-    .directive('livechat', function() {
-      return {
-        scope: {
-          license: '@'
-        },
-        bindToController: true,
-        controllerAs: 'vm',
-        controller: ['$window', function($window) {
-
+  angular.module('ngLiveChat', []).directive('livechat', function() {
+    return {
+      scope: {
+        license: '@'
+      },
+      bindToController: true,
+      controllerAs: 'vm',
+      controller: [
+        '$window',
+        function($window) {
           $window.__lc = __lc || {};
           $window.__lc.license = this.license;
 
           (function() {
-            var lc = $window.document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
-            lc.src = ('https:' == $window.document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
-            var s = $window.document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+            var lc = $window.document.createElement('script');
+            lc.type = 'text/javascript';
+            lc.async = true;
+            lc.src = 'https://cdn.livechatinc.com/tracking.js';
+            var s = $window.document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(lc, s);
           })();
-
-        }]
-      };
-    });
+        }
+      ]
+    };
+  });
 
   return angular.module('ngLiveChat');
-}));
+});
